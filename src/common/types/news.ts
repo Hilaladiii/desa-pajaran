ede9e75fb1b09e85e40048295c5f1d120a38bd35
Hilaladiii <abdulhilal242@gmail.com>
2024-07-13 10:14:17 +0700

    feat : add schema & type for news

---
+++ src/common/types/news.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+const MAX_FILE_SIZE = 2000000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+export const newsSchema = z.object({
+  id: z.string().optional(),
+  image: z
+    .any()
+    .refine((files) => files?.length == 1, "Image is required.")
+    .refine(
+      (files) => files?.[0]?.size <= MAX_FILE_SIZE,
+      `Max file size is 2MB.`
+    )
+    .refine(
+      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+      ".jpg, .jpeg, .png and .webp files are accepted."
+    ),
+  content: z.string(),
+  date: z.string().optional(),
+});
+
+export type TNews = z.infer<typeof newsSchema>;
