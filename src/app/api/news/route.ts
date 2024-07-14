e72f56e3e1158871d34c9230d6934eb66954119f
Hilaladiii <abdulhilal242@gmail.com>
2024-07-14 21:29:13 +0700

    feat : api route get news by id

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -1,7 +1,13 @@
-import { getNews } from "@/common/db/news";
-import { NextResponse } from "next/server";
+import { getNews, getNewsById } from "@/common/db/news";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const res = await getNews();
-  return NextResponse.json(res, { status: res.status });
+export async function GET(req: NextRequest) {
+  const id = await req.nextUrl.searchParams.get("id");
+  if (id) {
+    const res = await getNewsById(id);
+    return NextResponse.json(res, { status: res.status });
+  } else {
+    const res = await getNews();
+    return NextResponse.json(res, { status: res.status });
+  }
 }
