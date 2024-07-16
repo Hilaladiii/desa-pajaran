fd6779684fbd959e4a5977fba216fece7db36a70
Hilaladiii <abdulhilal242@gmail.com>
2024-07-16 19:53:11 +0700

    feat  : add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/(pages)/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/common/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        blue40: "#0088CC",
+        blue35: "#0077B3",
+        grey50: "#777788",
+        grey5: "#E6F1F6",
+        grey70: "#ADADB8",
       },
     },
   },
